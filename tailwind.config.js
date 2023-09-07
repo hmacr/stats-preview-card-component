675d3484e4661c23c05f298019c75cb56e80eb83
hmacr <hmac.devo@gmail.com>
2023-09-07 21:05:54 +0530

    setup tailwindcss colors, typography, and bg-img

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,28 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html'],
   theme: {
-    extend: {},
+    colors: {
+      'dark-blue': 'hsl(233, 47%, 7%)',
+      'desaturated-blue': 'hsl(244, 38%, 16%)',
+      'soft-violet': 'hsl(277, 64%, 61%)',
+      white: {
+        600: 'hsla(0, 0%, 100%, 0.6)',
+        700: 'hsla(0, 0%, 100%, 0.75)',
+        900: 'hsl(0, 0%, 100%)',
+      },
+    },
+    fontFamily: {
+      inter: ['Inter', ...defaultTheme.fontFamily.serif],
+      'lexend-deca': ['Lexend Deca', ...defaultTheme.fontFamily.serif],
+    },
+    extend: {
+      backgroundImage: {
+        'header-desktop': 'url("./images/image-header-desktop.jpg")',
+        'header-mobile': 'url("./images/image-header-mobile.jpg")',
+      },
+    },
   },
 };
